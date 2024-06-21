33b3c6eba65d59dbc256f9dc540a0918e0069d6e
Ian Davis <idavis@reliaquest.com>
2024-06-21 12:12:47 -0400

    Add telescope find text. Fix bug with makesymlinks.sh

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,6 @@ require('scripts')
 require("lazy").setup("plugins")
 vim.wo.number = true
 vim.go.syntax = "on"
+vim.go.tabstop = 4
+vim.go.shiftwidth = 4
+vim.go.expandtab = true
--- scripts/makesymlinks.sh
+++ scripts/makesymlinks.sh
@@ -10,14 +10,14 @@ dir=~/dotfiles                    # dotfiles directory
 oldFilesDir=~/dotfiles_old             # old dotfiles backup directory
 oldConfigsDir=~/config_old
 files=".p10k.zsh"    # list of files/folders to symlink in homedir
-configdirs="kitty neofetch nvim sketchybar borders"  
+configDirs="kitty neofetch nvim sketchybar borders"  
 
 
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+echo -n "Creating $oldFilesDir for backup of any existing dotfiles in ~ ..."
+mkdir -p $oldFilesDir
 echo "done"
 
 # change to the dotfiles directory
@@ -25,18 +25,22 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the configdir to any files in the ~/dotfiles directory specified in $configdirs
-for configdir in $configdirs; do
-    echo "Moving any existing config dirs from ~ to $oldConfigsDir"
-    mv ~/.config/$configdir $oldConfigsDir
-    echo "Creating symlink to $configdir in config directory."
-    ln -s $dir/$configdir ~/.config/$configdir
+# move any existing config files to ~/config_old directory, then create symlinks from the configDir to any files in the ~/.config directory specified in $configDirs
+echo "Moving any existing config dirs from ~/.config/ to $oldConfigsDir"
+for configDir in $configDirs; do
+    if [ -L ~/.config/$configDir ]; then
+        echo "~/.config/$configDir is a symlink so won't move."
+    else 
+        echo "~/.config/$configDir is not a symlink so will move."
+        mv ~/.config/$configDir $oldConfigsDir
+        echo "Creating symlink to ~/.config/$configDir"
+        ln -s $dir/$configDir ~/.config/$configDir
+    fi
 done
 
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the configdir to any files in the ~/dotfiles directory specified in $configdirs
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the configDir to any files in the ~/dotfiles directory specified in $configDirs
+echo "Moving any existing dotfiles from ~ to $oldFilesDir"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $oldFilesDir"
     mv ~/$file $oldFilesDir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
