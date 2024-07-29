5bb69001fd1947081d06c6bb53899379c73cbb70
Ian Davis <idavis@reliaquest.com>
2024-07-29 15:19:40 -0400

    idk

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,8 @@
 require('scripts')
 require("lazy").setup("plugins")
 vim.wo.number = true
+vim.wo.wrap = false
+vim.wo.relativenumber = true
 vim.go.syntax = "on"
 vim.go.tabstop = 4
 vim.go.shiftwidth = 4
