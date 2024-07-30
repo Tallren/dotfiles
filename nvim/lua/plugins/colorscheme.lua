614526ee6fd4877e88ac9b8026d58482e97d1f97
Ian Davis <idavis@reliaquest.com>
2024-07-30 11:31:11 -0400

    Fix colorscheme loading to use config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  }
+  { 
+  "ellisonleao/gruvbox.nvim", 
+  lazy = false,
+  priority = 1000,
+  config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme gruvbox]])
+    end,
+  },
 }
