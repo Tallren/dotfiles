27b8c44da0ab87d6a12d0c6d37751eae40b653a6
Ian Davis <idavis@reliaquest.com>
2024-08-02 10:13:57 -0400

    Include hidden files but ignore git folder with telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	keys = {
-		{ "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+		{ "<leader>pf", "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", desc = "Find Files" },
 		{ "<C-p>", "<cmd>Telescope git_files<cr>", desc = "Find Git Files" },
 		{ "<leader>pt", "<cmd>Telescope live_grep<cr>", desc = "Find Text" },
 		{
