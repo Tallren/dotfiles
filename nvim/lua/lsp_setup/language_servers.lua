83b8c5be3012a79a60fc3405bbffbf1ff2367e61
Ian Davis <idavis@reliaquest.com>
2024-08-09 13:10:46 -0400

    LSP: Use pyright

--- nvim/lua/lsp_setup/language_servers.lua
+++ nvim/lua/lsp_setup/language_servers.lua
@@ -3,7 +3,7 @@ require("mason-lspconfig").setup({
     ensure_installed = {
       "clangd",
       "lua_ls",
-      "pylsp",
+      "pyright",
       "tsserver",
       "yamlls",
       "marksman",
