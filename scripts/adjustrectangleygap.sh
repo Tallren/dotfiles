3daf7e541cfa80072391f7ee3fd80119905c5db8
Ian <iandavisdev@gmail.com>
2025-04-03 10:24:30 -0400

    Rectangle: Use screenEdgeGapTopNotch

--- scripts/adjustrectangleygap.sh
+++ scripts/adjustrectangleygap.sh
@@ -3,6 +3,6 @@
 ##########
 # This script adjusts Rectangle's gap at the top of the screen to account for sketchybar's placement.##########
 #
-defaults write com.knollsoft.Rectangle screenEdgeGapTop -int 25
+defaults write com.knollsoft.Rectangle screenEdgeGapTopNotch -int 1
 killall Rectangle
 open /Applications/Rectangle.app
